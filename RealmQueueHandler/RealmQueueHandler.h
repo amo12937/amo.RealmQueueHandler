ad04bd59fbfa00abfa212fe1253100b3ea04ad59
s_anno <s_anno@spica.co.jp>
2015-01-19 14:32:42 +0900

    Initial Commit

---
+++ RealmQueueHandler/RealmQueueHandler.h
@@ -0,0 +1,19 @@
+//
+//  RealmQueueHandler.h
+//  RealmQueueHandler
+//
+//  Created by 安野周太郎 on 2015/01/19.
+//  Copyright (c) 2015年 amo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RealmQueueHandler.
+FOUNDATION_EXPORT double RealmQueueHandlerVersionNumber;
+
+//! Project version string for RealmQueueHandler.
+FOUNDATION_EXPORT const unsigned char RealmQueueHandlerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RealmQueueHandler/PublicHeader.h>
+
+
