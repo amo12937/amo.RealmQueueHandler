d0268b4928a508feef5e1027e050563372348e10
s_anno <s_anno@spica.co.jp>
2015-01-19 21:49:14 +0900

    added realm as a submodule

---
+++ RealmQueueHandler/src/RealmQueueHandler.swift
@@ -0,0 +1,13 @@
+//
+//  RealmQueueHandler.swift
+//  RealmQueueHandler
+//
+//  Created by 安野周太郎 on 2015/01/19.
+//  Copyright (c) 2015年 amo. All rights reserved.
+//
+
+import Foundation
+import Realm
+
+
+
